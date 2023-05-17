01c80a704790731752c2b90ffb6183fe1c053be8
Nishant Singh <nishant.singh1@BLRETV-LECPMD6M.local>
2023-05-17 17:30:56 +0530

    first commit

---
+++ src/com/flipkart/demo/Demo.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author nishant.singh1
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is My First Program");
+
+	}
+
+}
